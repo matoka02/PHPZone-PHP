a20742384d006f72457d51bdc9889d45d7c032a8
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-02-28 21:16:42 +0200

    lesson-22

---
+++ www/L-22/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/*---Learning to work with cookies in PHP---*/
+
+// Ввести в браузер для отслеживания
+// http://myproject.loc/L-22/viewCookies.php
+
+// Ввести в браузер для отправки
+// http://myproject.loc/L-22/setCookies.php
---
+++ www/L-22/viewCookies.php
@@ -0,0 +1,5 @@
+<?php
+var_dump($_COOKIE);
+// C:\OSPanel\domains\myproject.loc\www\L-22\viewCookies.php:2:
+// array (size=0)
+//  empty
