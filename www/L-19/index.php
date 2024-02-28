254826906aebb54c778511b7dfc1f3ec59e7a425
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-02-28 13:50:24 +0200

    lesson-19

---
+++ www/L-19/index.php
@@ -0,0 +1,63 @@
+<?php
+
+/*---GET requests in PHP---*/
+
+//echo 'Hello world!';
+
+// Ввести в браузер
+// http://myproject.loc/L-19/index.php?arg1=123&arg2=scrrr
+//var_dump($_GET);
+
+// Ввести в браузер
+// http://myproject.loc/L-19/index.php?login=admin&password=12345
+//var_dump($_GET);
+//echo $_GET['login'];
+$login = !empty($_GET['login']) ? $_GET['login'] : 'логин не передан!';
+$password = !empty($_GET['password']) ? $_GET['password'] : 'пароль не передан!';
+?>
+
+<html>
+<head>
+  <title>Знакомство с GET-запросами</title>
+</head>
+<body>
+<p>
+  Переданный логин: <?= $login ?>
+  <br>
+  Переданный пароль: <?= $password ?>
+</p>
+</body>
+</html>
+
+<?php
+// Ввести в браузер
+// http://myproject.loc/L-19/index.php
+echo '<hr>';
+?>
+
+<html>
+<head>
+  <title>Форма входа</title>
+</head>
+<body>
+<form action="/L-19/login.php" method="get">
+  <label>
+    Логин <input type="text" name="login">
+  </label>
+  <br>
+  <label>
+    Пароль <input type="password" name="password">
+  </label>
+  <br>
+  <input type="submit" value="Войти">
+</form>
+</body>
+</html>
+
+<?php
+// admin
+// Pa$$w0rd
+// Ввести в браузер
+// http://myproject.loc/L-19/login.php?login=admin&password=Pa%24%24w0rd;
+echo '<hr>';
+
---
+++ www/L-19/login.php
@@ -0,0 +1,45 @@
+<?php
+$login = !empty($_GET['login']) ? $_GET['login'] : '';
+$password = !empty($_GET['password']) ? $_GET['password'] : '';
+
+if ($login === 'admin' && $password === 'Pa$$w0rd') {
+  $isAuthorized = true;
+} else {
+  $isAuthorized = false;
+}
+?>
+<html>
+<head>
+  <title>Результат авторизации</title>
+</head>
+<body>
+<p>
+  <?= $isAuthorized ? 'Логин и пароль верные!' : 'Неправильный логин или пароль' ?>
+</p>
+</body>
+</html>
+
+<?php
+/*Homework*/
+$login = $_GET['login'] ?? '';
+$password = $_GET['password'] ?? '';
+
+if ($login === 'admin' && $password === 'Pa$$w0rd') {
+  $authResult = 'Авторизация прошла успешно';
+} else if($login !== 'admin') {
+  $authResult = 'Логин неверный';
+} else {
+  $authResult = 'Пароль неверный';
+}
+?>
+
+<html>
+<head>
+  <title>Результат авторизации</title>
+</head>
+<body>
+<?= $authResult ?>
+</body>
+</html>
+
+
