6e682877bd87eb5aef2f9afd7b0b461f6f85c511
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-03-13 17:26:33 +0200

    oop-lesson-09

---
+++ www/EL-09/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/*---Namespaces and autoloading in PHP---*/
+
+
+require __DIR__ . '/src/MyProject/Models/Users/User.php';
+require __DIR__ . '/src/MyProject/Models/Articles/Article.php';
+$author = new User('Иван');
+$article = new Article('Заголовок', 'Текст', $author);
+var_dump($article);
