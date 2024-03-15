61ac3929a54f0e8b68076ed103a06a198caac7a6
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-03-15 18:43:54 +0200

    oop-lesson-14

---
+++ www/EL-14/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/*---Creating a database for a future blog---*/
+
+// Перейти в браузере
+// http://127.0.0.1/openserver/phpmyadmin/index.php
+
+// Открываем вкладку «Базы данных». Вводим имя нашей новой базы - my_project, и выбираем сравнение utf8_general_ci.
+// Дамп базы в файле dump.sql.
