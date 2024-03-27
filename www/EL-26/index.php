2cf6a874820f4b519bfaffd4268c72030ae4181b
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-03-27 16:02:05 +0200

    update

--- www/EL-26/index.php
+++ www/EL-26/index.php
@@ -5,6 +5,9 @@
 // Перейти в браузере
 // http://127.0.0.1/openserver/phpmyadmin/index.php
 
+// Перейти в браузере
+// http://myproject.loc/EL-26/example.php
+
 // spl_autoload_register(function (string $className) {
 //   require_once __DIR__ . '/../../' . str_replace('\\', '/', $className) . '.php';
 // });
@@ -53,6 +56,8 @@ try {
   }
 
   if (!$isRouteFound) {
+    // echo 'Страница не найдена!';
+    // return;
     throw new src\MyProject\EL_26\Exceptions\NotFoundException();
   }
 
@@ -63,7 +68,8 @@ try {
 
   $controller = new $controllerName();
   $controller->$actionName(...$matches);
-} catch (\src\MyProject\EL_26\Exceptions\DbException $e) {
+} catch (\src\MyProject\EL_27\Exceptions\DbException $e) {
+  // echo $e->getMessage();
   $view = new \src\MyProject\EL_26\View\View(__DIR__ . '/../../src/templates/EL_26/errors');
   $view->renderHtml('500.php', ['error' => $e->getMessage()], 500);
 } catch (\src\MyProject\EL_26\Exceptions\NotFoundException $e) {
