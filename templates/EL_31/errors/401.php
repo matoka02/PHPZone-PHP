0f8be3e9cf34ef1105c38ea8167c0effc0a05011
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-04-21 13:52:18 +0300

    fix

--- templates/EL_31/errors/401.php
+++ templates/EL_31/errors/401.php
@@ -1,6 +1,6 @@
 <?php include __DIR__ . '/../header.php'; ?>
 
 <h1>Вы не авторизованы</h1>
-Для доступа к этой странице нужно <a href="/EL-30/users/login">войти на сайт</a>
+Для доступа к этой странице нужно <a href="/EL-31/users/login">войти на сайт</a>
 
 <?php include __DIR__ . '/../footer.php'; ?>
